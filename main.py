1c7618b4d7efb987ccf51e9c3d31326c249aeef3
NEVI0 <neviocostamagagnin@gmail.com>
2021-06-01 09:05:14 -0300

    Fixed game over screen and added new coments

--- main.py
+++ main.py
@@ -35,38 +35,47 @@ game_obj: Tetris = Tetris(20, 10) # Initialize Tetris Object
 # Game Over Screen
 def game_over() -> None:
 
-    GAME_SCREEN.fill((255, 255, 255))  # Deixa a tela branca
+    GAME_SCREEN.fill((255, 255, 255)) # Set a blank screen
 
-    # Game Over Message
-    end_message = GAME_FONT.render(f'Game Over! You got {game_obj.score} points. Press ESC to restart!', True, (0, 0, 0)) 
-    end_message_rect = end_message.get_rect()
-    end_message_rect.center = (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2) # Centers message at the middle of screen
+    game_over_text = GAME_FONT.render(f'Game Over!', True, (0, 0, 0)) # Game over text
+    points_text = GAME_FONT.render(f'You have got {game_obj.score} points', True, (0, 0, 0)) # Points text
+    restart_text = GAME_FONT.render(f'Press ESC to restart', True, (0, 0, 0)) # Restart text
 
-    # Game Events
+    # Centers game over text at the middle of screen
+    game_over_rect = game_over_text.get_rect();
+    game_over_rect.center = (WINDOW_SIZE[0] // 2, 180)
+
+    # Centers points text at the middle and bellow of game over text
+    points_rect = points_text.get_rect();
+    points_rect.center = (WINDOW_SIZE[0] // 2, 220)
+
+    # Centers points restart text at the middle and bellow of points text
+    restart_rect = restart_text.get_rect();
+    restart_rect.center = (WINDOW_SIZE[0] // 2, 260)
+
+    GAME_SCREEN.blit(game_over_text, game_over_rect)
+    GAME_SCREEN.blit(points_text, points_rect)
+    GAME_SCREEN.blit(restart_text, restart_rect)
+
+    # Game events
     for e in pygame.event.get():
         if e.type == QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit() # Quit game
 
         if e.type == KEYDOWN:
             if e.key == K_ESCAPE:
-                # restart() Restart Game
                 game_obj.__init__(20, 10) # Restart Game
 
-    GAME_SCREEN.blit(end_message, end_message_rect) # Renders Game Over Message
     pygame.display.update() # Update Game Screen
 
 # Game Loop
 while not done:
 
-    if game_obj.state == 'gameover':
-        game_over()
+    points_text = GAME_FONT.render(f'Points: {game_obj.score}', True, (0, 0, 0)) # Points text
 
-    points_text = GAME_FONT.render(f'Pontos: {game_obj.score}', True, (0, 0, 0)) # Points Text
-    points_text_rect = points_text.get_rect()
-    points_text_rect.center = (0, 0)
-
-    GAME_SCREEN.fill((255, 255, 255))  # White Background Color
+    GAME_SCREEN.fill((255, 255, 255))  # White background color
+    GAME_SCREEN.blit(points_text, (10, 10)); # Renders points text at the top
 
     # Renders a new block if none
     if game_obj.block is None:
@@ -78,40 +87,33 @@ while not done:
     if counter > 10000000:
         counter = 0
 
+    # Makes block fall down
     if counter % (FPS // game_obj.level // 2) == 0 or pressing_down:
         if game_obj.state == 'start':
-            game_obj.go_down()
+            game_obj.go_down() # Falls down if is not game over state
 
-    # Game Events
+    # Game events
     for ev in pygame.event.get():
-        # Quit Event
-        if ev.type == QUIT:
+        if ev.type == QUIT: # Quit event
             done = True
 
-        # Keyboard Events
-        if ev.type == KEYDOWN:
+        if ev.type == KEYDOWN: # Keyboard events
             if ev.key == K_UP:
-                game_obj.rotate()
-
+                game_obj.rotate() # Rotate block
             if ev.key == K_DOWN:
-                pressing_down = True
-
+                pressing_down = True # Increase block speed
             if ev.key == K_LEFT:
-                game_obj.go_side(-1)
-
+                game_obj.go_side(-1) # Goes left
             if ev.key == K_RIGHT:
-                game_obj.go_side(1)
-
+                game_obj.go_side(1) # Goes right
             if ev.key == K_SPACE:
-                game_obj.go_space()
-
+                game_obj.go_space() # Goes down
             if ev.key == K_ESCAPE:
-                game_obj.__init__(20, 10)
+                game_obj.__init__(20, 10) # Restart game
 
-        # Checks if the user is typing KEYUP
-        if ev.type == pygame.KEYUP:
+        if ev.type == pygame.KEYUP: # Keyup event
             if ev.key == pygame.K_DOWN:
-                pressing_down = False # Then, just move
+                pressing_down = False # Reset block speed
 
     for i in range(game_obj.height):
         for j in range(game_obj.width):
@@ -130,7 +132,10 @@ while not done:
                                       game_obj.y + game_obj.zoom * (i + game_obj.block.y) + 1,
                                       game_obj.zoom - 2, game_obj.zoom - 2])
 
-    pygame.display.flip()
-    TIMER.tick(FPS) # Runs Game in 60 FPS
+    if game_obj.state == 'gameover':
+        game_over() # Renders game over screen
+
+    pygame.display.flip() # Update the full display surface to the screen
+    TIMER.tick(FPS) # Runs the game in 60 FPS
 
 pygame.quit()
